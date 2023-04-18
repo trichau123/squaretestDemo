8437f83f61b9c92e158ff8f72b8a4de830cb8017
trichau123 <53028788+trichau123@users.noreply.github.com>
2023-04-18 16:07:37 -0400

    first commit

---
+++ src/test/java/com/example/squaretestdemo/SquareTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squaretestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
